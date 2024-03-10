c121d5ea0cc4401a5c3e751c0cb2822c927ce467
Wenxin Mao <maowenxin72@yahoo.com>
2024-03-10 11:20:52 -0500

    initial commit

---
+++ src/Fabricjs.tsx
@@ -0,0 +1,4 @@
+const Fabricjs = ()=>{
+    return <>hi</>;
+};
+export default Fabricjs;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import Fabricjs from "./Fabricjs";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Home</h1>
+        <Link to="about">Fabricjs</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <Fabricjs />
+  },
+]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+createRoot(document.getElementById("root") as any).render(
+  <RouterProvider router={router} />
+);
