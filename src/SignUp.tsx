66cef2ecb6632f6672f6a738f8ceb9272e7eba69
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-12 17:33:36 -0400

    Update SignUp.tsx

--- src/SignUp.tsx
+++ src/SignUp.tsx
@@ -1,7 +1,7 @@
 
 // SignUp.tsx
 import React, { useState } from 'react';
-import { TextField, Button, Container, Typography, Box, Link } from '@mui/material';
+import { TextField, Button, Container, Typography, Box, Link, CheckBox } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
