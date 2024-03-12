2c09dc105704e29f287ea8fe94d338cb3c7eb4ee
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-12 12:52:10 -0400

    Create Login.tsx

---
+++ src/Login.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { TextField, Button, Container, Typography, Box } from '@mui/material';
+
+const LoginPage: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Implement your login logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Reset fields after logging in
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Box mt={5}>
+        <Typography variant="h4" align="center">Login</Typography>
+        <form onSubmit={handleLogin}>
+          <TextField
+            label="Username"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            label="Password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button variant="contained" color="primary" fullWidth type="submit">
+            Login
+          </Button>
+        </form>
+      </Box>
+    </Container>
+  );
+};
+
+export default LoginPage;
