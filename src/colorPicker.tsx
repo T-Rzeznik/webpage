7438a9e05cecfaeae36614318579c3d2d88c1b69
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-22 13:13:28 -0400

    Update colorPicker.tsx

--- src/colorPicker.tsx
+++ src/colorPicker.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
 import { Slider, InputLabel, FormControl, Box } from '@mui/material';
 import { SelectChangeEvent, MenuItem, Popover, Paper, Typography } from '@mui/material';
