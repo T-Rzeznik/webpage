28629a3a444eae812529547d83fc6317874a0aad
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-22 13:03:19 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <div>
+       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
         <h1>Home</h1>
         <Link to="about">Fabricjs</Link>
         
