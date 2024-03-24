79f05a0ae588c372e036e33a0ff81e3933a73a24
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-24 15:46:50 -0400

    Create LargeRectangle.tsx

---
+++ src/LargeRectangle.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface LargeRectangleProps {
+  title: string;
+  description: string;
+}
+
+const LargeRectangle: React.FC<LargeRectangleProps> = ({ title, description }) => {
+  return (
+    <div className="large-rectangle">
+      <div className="top" />
+      <div className="bottom">
+        <div className="title">{title}</div>
+        <div className="description">{description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default LargeRectangle;
