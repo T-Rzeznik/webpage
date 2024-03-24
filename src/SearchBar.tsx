41e896ea1359744151ddf904e6054d50b42c1f81
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-24 15:20:36 -0400

    Delete src/SearchBar.tsx

--- src/SearchBar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SearchBar: React.FC = () => {
-  return (
-    <input type="text" placeholder="Search..." className="search-input" />
-  );
-};
-
-export default SearchBar;
