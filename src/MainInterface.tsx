7183b1e1b5bd9debd68cca85277625e5b6b3ee5f
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-24 16:48:02 -0400

    Update MainInterface.tsx

--- src/MainInterface.tsx
+++ src/MainInterface.tsx
@@ -1,18 +1,22 @@
+// MainInterface.tsx
 import React from 'react';
-import './MainInterface.css';
 import TopBar from './TopBar';
 import LargeRectangle from './LargeRectangle';
 
-const MainInterface: React.FC = () => {
+const MainInterface = () => {
   return (
     <div className="main-interface">
       <TopBar />
+      <h1 className="main-title">All Boards</h1>
       <div className="rectangle-container">
         <LargeRectangle title="Title 1" description="Description 1" />
         <LargeRectangle title="Title 2" description="Description 2" />
         <LargeRectangle title="Title 3" description="Description 3" />
         <LargeRectangle title="Title 4" description="Description 4" />
         <LargeRectangle title="Title 5" description="Description 5" />
+        <LargeRectangle title="Title 6" description="Description 6" />
+        <LargeRectangle title="Title 7" description="Description 7" />
+        <LargeRectangle title="Title 8" description="Description 8" />
       </div>
     </div>
   );
