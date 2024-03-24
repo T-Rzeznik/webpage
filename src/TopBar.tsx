48decaa4e66111abb6792731babb858417e2a42f
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-24 15:30:47 -0400

    Update TopBar.tsx

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -1,12 +1,21 @@
-// TopBar.tsx
 import React from 'react';
+import './TopBar.css';
 
 const TopBar: React.FC = () => {
   return (
     <div className="top-bar">
-      <button className="dropdown-button">Menu ▼</button>
-      <input type="text" placeholder="Search..." className="search-bar" />
-      <div className="profile-avatar">Profile</div>
+      <div className="dropdown">
+        <button className="dropdown-button">SoTeaching ▼</button>
+        <div className="dropdown-content">
+          <a href="#">SOteaching</a>
+          <a href="#">Switch Team</a>
+          <a href="#">Create Team</a>
+        </div>
+      </div>
+      <div className="search-profile">
+        <input type="text" placeholder="Search..." className="search-bar" />
+        <div className="profile-avatar">Profile</div>
+      </div>
     </div>
   );
 };
