8faf11c4d9aec13d93bf8d2e33db22220ef2e0d3
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-25 15:58:52 -0400

    Update MainInterface.tsx

--- src/MainInterface.tsx
+++ src/MainInterface.tsx
@@ -1,21 +1,20 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import './MainInterface.css'; // Import CSS for MainInterface
-import TopBar from './TopBar'; // Import TopBar component
-import LargeRectangle from './LargeRectangle'; // Import LargeRectangle component
+import './MainInterface.css'; 
+import TopBar from './TopBar'; 
+import LargeRectangle from './LargeRectangle';
 
 const MainInterface: React.FC = () => {
   return (
     <div className="main-interface">
-      {/* Render TopBar component */}
+  
       <TopBar />
-
-      {/* Render Allboards H1 aligned right */}
+    
       <h1 className="all-boards">All Boards</h1>
 
-      {/* Parent container for LargeRectangle components */}
+     
       <div className="rectangle-container">
-        {/* Render multiple LargeRectangle components */}
+       
         <LargeRectangle title="Board 1" description="Description 1" />
         <LargeRectangle title="Board 2" description="Description 2" />
         <LargeRectangle title="Board 3" description="Description 3" />
@@ -25,7 +24,7 @@ const MainInterface: React.FC = () => {
         <LargeRectangle title="Board 7" description="Description 7" />
         <LargeRectangle title="Board 8" description="Description 8" />
         <LargeRectangle title="Board 9" description="Description 9" />
-        {/* Add more LargeRectangle components as needed */}
+       
       </div>
     </div>
   );
