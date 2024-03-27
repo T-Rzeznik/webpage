8bacca98160d913ef07610798bb65c5df04b5ae1
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-27 15:51:04 -0400

    Create MainInterface2.tsx

---
+++ src/Interface/MainInterface2.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { PageContainer } from 'joy-ui';
+import TopBar from './TopBar';
+import LargeRectangle from './LargeRectangle';
+
+const MainInterface2: React.FC = () => {
+  return (
+    <PageContainer>
+      <TopBar />
+      <h1 className="all-boards">All Boards</h1>
+      <div className="rectangle-container">
+        <LargeRectangle title="Board 1" description="Description 1" />
+        <LargeRectangle title="Board 2" description="Description 2" />
+        <LargeRectangle title="Board 3" description="Description 3" />
+        <LargeRectangle title="Board 4" description="Description 4" />
+        <LargeRectangle title="Board 5" description="Description 5" />
+        <LargeRectangle title="Board 6" description="Description 6" />
+        <LargeRectangle title="Board 7" description="Description 7" />
+        <LargeRectangle title="Board 8" description="Description 8" />
+        <LargeRectangle title="Board 9" description="Description 9" />
+      </div>
+    </PageContainer>
+  );
+};
+
+export default MainInterface2;
