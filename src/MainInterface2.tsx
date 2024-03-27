b2afdd0887764645c65cca0d3b7a5c65d8052cd7
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-27 16:52:05 -0400

    Update MainInterface2.tsx

--- src/MainInterface2.tsx
+++ src/MainInterface2.tsx
@@ -1,39 +1,45 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, InputBase, IconButton, Avatar, Box, Grid, Paper } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, InputBase, IconButton, Avatar, Box, Grid, Paper, Menu, MenuItem } from '@mui/material';
 import { Menu as MenuIcon, Search as SearchIcon } from '@mui/icons-material';
 
 const MainInterface2: React.FC = () => {
-  const renderBoards = () => {
-    const boards = [];
-    for (let index = 0; index < 9; index++) {
-      boards.push(
-        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-          <Paper sx={{ p: 2 }}>
-            <Typography variant="h6" sx={{ mb: 1 }}>Board {index + 1}</Typography>
-            <Typography variant="body1" sx={{ color: 'text.secondary' }}>Description {index + 1}</Typography>
-          </Paper>
-        </Grid>
-      );
-    }
-    return boards;
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ bgcolor: 'grey.200' }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" noWrap sx={{ flexGrow: 1 }}>
             SoTeaching
           </Typography>
+          <IconButton
+            size="large"
+            aria-label="open dropdown"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenuClick}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>SoTeaching</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Switch Team</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Create Team</MenuItem>
+          </Menu>
           <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}>
             <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}>
               <IconButton sx={{ p: '10px' }} aria-label="search">
@@ -59,7 +65,6 @@ const MainInterface2: React.FC = () => {
           </Box>
         </Toolbar>
       </AppBar>
-      {/* {renderMenu} */}
       <Typography variant="h4" sx={{ mt: 2, ml: 2 }}>All Boards</Typography>
       <Grid container spacing={2} sx={{ p: 2 }}>
         {renderBoards()}
