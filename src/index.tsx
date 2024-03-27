bf785c45646359153af1808a34e58372f2d3f2a3
T-Rzeznik <141183295+T-Rzeznik@users.noreply.github.com>
2024-03-27 16:28:41 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import SignUp from './SignUp';
 import CreateTeamPage from './CreateTeam';
 import ColorPicker from './colorPicker';
 import MainInterface from './MainInterface';
-import MainInterface2 from './Interface/MainInterface2';
+import MainInterface2 from './MainInterface2';
 
 const router = createBrowserRouter([
   {
